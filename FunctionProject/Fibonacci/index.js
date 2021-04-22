c88adc6826e435adf6661f7d13a796bfa8778bde
Jairo Pulido <jairo.pulido@mail.escuelaing.edu.co>
2021-04-22 13:31:07 -0500

    Implementación punto 6 mem

--- FunctionProject/Fibonacci/index.js
+++ FunctionProject/Fibonacci/index.js
@@ -1,4 +1,7 @@
 var bigInt = require("big-integer");
+
+var mem = [];
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
@@ -14,14 +17,20 @@ module.exports = async function (context, req) {
     else if (nth === 1)
         answer = nth_1
     else {
-        for (var i = 0; i < nth - 1; i++) {
-            answer = nth_2.add(nth_1)
-            nth_2 = nth_1
-            nth_1 = answer
-        }
-    }
-
+        if(nth+1>mem.length){
+            mem = [];
+            for(var i=0;i<nth+1;i++){
+                mem.push(-1);}}
+        answer = recursiveFibonacci(nth);}
     context.res = {
-        body: answer.toString()
-    };
-}
+        body: answer.toString()};}
+
+function recursiveFibonacci(n){
+    if(n==0 || n==1){
+        return bigInt.one;
+    }else{
+        if(mem[n]!=-1) { 
+			return mem[n];}
+			else {
+			mem[n] = recursiveFibonacci(n-1).add(recursiveFibonacci(n-2));
+			return mem[n];}}}
